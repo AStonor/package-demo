b439526a548aa250b15d9b76fa5077631c663eab
Andreas Stonor Kristensen <Andreas.Kristensen@ufst.dk>
2021-08-11 09:50:26 +0200

    test

---
+++ src/main/java/Demo/PrintDemo.java
@@ -0,0 +1,19 @@
+package Demo;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class PrintDemo {
+
+
+    public static void PrintOne(){
+        System.out.println("1");
+    }
+
+    public static void PrintTwo(){
+        System.out.println("2");
+    }
+
+    public static void PrintThree(){
+        System.out.println("3");
+    }
+}
